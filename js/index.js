5c783427a4bbdfb330a996327c1f78e1f0ba4b2e
other-liam <44048562+other-liam@users.noreply.github.com>
2018-11-27 09:24:50 +1000

    Add files via upload
    Removed hardcoded time values and replaced with global variables, and a function to convert from 24-hour time to seconds (calculateShiftTime). Also cleaned up comments.

--- js/index.js
+++ js/index.js
@@ -2,64 +2,80 @@ $('.message a').click(function(){
    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
 });
 
-//SHIFT TIMES
-var shiftOneStartTime = 200; //can't use 0200 because JS hates leading 0s
+// SHIFT TIMES
+// can't use 0200 because JS hates leading 0s
+var shiftOneStartTime = 200;
 var shiftOneEndTime = 1700;
 var shiftTwoStartTime = 1700;
 var shiftTwoEndTime = 2000;
-var numberOfSecondsInDay = 60 * 60 * 24; //Tesys sets 12 AM Sunday as reference point
 
-//TESYS COMMANDS
-//CUT BACK REALTIME SHIFT TO 2:00 - 17:00, ADD PRMT3 PROPERTY
+// Tesys sets 12 AM Sunday as reference point. i.e. 2 AM Monday is +26 hours, and so on.
+var numberOfSecondsInDay = 60 * 60 * 24;
+                                         
+// TESYS COMMANDS
+// Cut back realtime shift to 2:00 - 17:00, add PRMT3 property
 var rtShiftCommand = 'realtimeShiftCommand -r"';
 var rtRegion = '" -d"AUSQLD ';
 var rtCutbackAndPermit = '" -R"AUSQLD 2:00 -AUSQLD 17:00" -!"PRMT3"';
 
-//CUT BACK SHIFT 1 TO 2:00 - 17:00
+// Cut back shift 1 to 2:00 - 17:00
 var calChange = 'resCalendar  -a"AddItem" -r"';
 var calDateChange = '" -d"';
 var calShiftOneChange = '" -s"AUSQLD,';
 var endAdjustment = '" -u"0"'
 
-//CREATE SHIFT 2 AS 17:00 - 20:00, ADD PRMT3 + NIGHT PROPERTIES
+// Create shift 2 as 17:00 - 20:00, add PRMT3 + NIGHT properties
 var calShiftTwoChange = '" -n"2" -s"AUSQLD,';
 var calPermits = '" -u"0" -c"NIGHT SHIFT" -H"1" -!"NIGHT,PRMT3"';
 
-//RE-USE ON
+// Re-use on
 var reuse = 'vc -v"';
 var reuseOn = '" -^"1"';
 
-//RE-USE OFF
+// Re-use off
 var reuseOff = '" -^"0"';
 
-//UNDO
+// Undo
 var rtUndo = '" -x"1" -!""'
 var calUndo = 'resCalendar  -a"UpdateItem" -r"'
 var calSecondShiftUndo = '" -n"2" -s"unset" -c"unset" -H"unset" -!"unset"'
 var calFirstShiftUndo = '" -n"1" -s"unset"'
 
-//DATE MAGIC
+// DATE MAGIC
 var today = new Date();
+
+// returns 1-7 for Monday to Sunday
 var dayNumber = today.getDay();
+
+// returns the day of the month, e.g. 27.
 var dd = today.getDate();
+
+// returns the current month, indexed at 0, so +1 for real answer
 var mm = today.getMonth() + 1;
+
+// returns the year, e.g. 2018.
 var yyyy = today.getFullYear();
 
-if (dd < 10) { // if the date is any single digit
-  dd = "0" + dd; // add a leading 0, so the day is always 2 digits long.
+// if the date is any single digit, add a leading 0, so the day is always 2 digits long.
+if (dd < 10) {
+  dd = "0" + dd;
 }
 
+// as above, but for the month value.
 if (mm < 10) {
   mm = "0" + mm;
 }
 
-today = dd + "." + mm + "." + yyyy;
-//END DATE MAGIC
+// join together, e.g. 27.11.2018.
+today = dd + "." + mm + "." + yyyy; 
+// END DATE MAGIC
 
-//START TIME CALCULATION MAGIC
-//Input a shift number, either 1 or 2, and return a two-item array of shift times.
+// START TIME CALCULATION MAGIC
+// Input a shift number, either 1 or 2, and return a two-item array of shift times.
 function calculateShiftTime(shiftNumber){
   if (shiftNumber == 1) {
+    
+    // calculate number of seconds since 12 AM Sunday until shift times.
     var timeArray = [(shiftOneStartTime * 36) + (numberOfSecondsInDay * dayNumber),
                      (shiftOneEndTime * 36) + (numberOfSecondsInDay * dayNumber)]
   }
@@ -68,33 +84,43 @@ function calculateShiftTime(shiftNumber){
                      (shiftTwoEndTime * 36) + (numberOfSecondsInDay * dayNumber)]
   }
   else console.log("Invalid shift number!")
+  
+  // returns an array of two times, start and finish, e.g. 93600,148700
   return timeArray
 }
-//END START TIME CALCULATION MAGIC
+// END START TIME CALCULATION MAGIC
 
-//SAVE PRMT3 FILE BUTTON
+// SAVE FILE BUTTON
 $("#btn-save").click(function() {
-
+  // get list of trucks from HTML input
   var unformattedTruckList = $("#input-truckList").val();
+  
+  // assume delimited by a space, split into array
   var truckArray = unformattedTruckList.split(" ");
+  
   for(var i=0;i<truckArray.length;i++){
+    // add 'PCA' prefix to each item in array for full fleet number.
     truckArray[i]="PCA"+truckArray[i];
   }
   
-  //CUT BACK REALTIME SHIFT
+  // CUT BACK REALTIME SHIFT
+  // copy truckArray into seperate array
   var arrayOfTrucksForRealtime = truckArray.slice(0);
+  
+  // pasting all the Tesys commands together with the fleet number.
   for (var i = 0; i < arrayOfTrucksForRealtime.length; i++) {
     arrayOfTrucksForRealtime[i] =
       rtShiftCommand +
-      arrayOfTrucksForRealtime[i] +
+      arrayOfTrucksForRealtime[i] +//
       rtRegion +
       today +
       rtCutbackAndPermit;
   }
   
+  // turn the array of Tesys commands into a list for exporting, delimited by a new line.
   var listOfRealtimeCutbackCommands = arrayOfTrucksForRealtime.join("\r\n");
   
-  //CUT BACK SHIFT 1
+  // CUT BACK SHIFT 1
   var arrayOfTrucksForShiftOne = truckArray.slice(0);
   for (var i = 0; i < arrayOfTrucksForShiftOne.length; i++) {
     arrayOfTrucksForShiftOne[i] =
@@ -103,13 +129,16 @@ $("#btn-save").click(function() {
       calDateChange +
       today +
       calShiftOneChange +
+      
+      // using the function to convert shift 1 times into seconds
       calculateShiftTime(1) +
+      
       endAdjustment;
   }
   
   var listOfShiftOneChangeCommands = arrayOfTrucksForShiftOne.join("\r\n");
   
-  //CREATE SHIFT 2
+  // CREATE SHIFT 2
   var arrayOfTrucksForShiftTwo = truckArray.slice(0);
   for (var i = 0; i < arrayOfTrucksForShiftTwo.length; i++) {
     arrayOfTrucksForShiftTwo[i] =
@@ -124,7 +153,7 @@ $("#btn-save").click(function() {
   
   var listOfShiftTwoCommands = arrayOfTrucksForShiftTwo.join("\r\n");
   
-  //RE-USE ON
+  // RE-USE ON
   var arrayOfTrucksForReuseOn = truckArray.slice(0);
   for (var i = 0; i < arrayOfTrucksForReuseOn.length; i++) {
     arrayOfTrucksForReuseOn[i] =
@@ -135,7 +164,7 @@ $("#btn-save").click(function() {
   
   var listOfReuseOnCommands = arrayOfTrucksForReuseOn.join("\r\n");
   
-  //RE-USE OFF
+  // RE-USE OFF
   var arrayOfTrucksForReuseOff = truckArray.slice(0);
   for (var i = 0; i < arrayOfTrucksForReuseOff.length; i++) {
     arrayOfTrucksForReuseOff[i] =
@@ -146,6 +175,7 @@ $("#btn-save").click(function() {
   
   var listOfReuseOffCommands = arrayOfTrucksForReuseOff.join("\r\n");
   
+  // join all lists into an array, then join that array, delimited by a new line.
   var outputArray = [
     listOfRealtimeCutbackCommands,
     listOfShiftOneChangeCommands,
@@ -158,7 +188,7 @@ $("#btn-save").click(function() {
   var blob = new Blob([outputArray], { type: "cmd" });
   saveAs(blob, filename + ".cmd");
   
-  //OUTPUT SECTION
+  // send contents of file to HTML output for easy viewing
   document.getElementById('result').innerHTML = outputArray;});
 
 $("#btn-undo").click(function() {
@@ -169,7 +199,7 @@ $("#btn-undo").click(function() {
     truckArray[i]="PCA"+truckArray[i];
   }
   
-  //UNDO CUT BACK REALTIME SHIFT
+  // undo the cutting back of the realtime shift
   var undoArrayOfTrucksForRealtime = truckArray.slice(0);
   for (var i = 0; i < undoArrayOfTrucksForRealtime.length; i++) {
     undoArrayOfTrucksForRealtime[i] =
@@ -182,7 +212,7 @@ $("#btn-undo").click(function() {
   
   var listOfUndoRealtimeCutbackCommands = undoArrayOfTrucksForRealtime.join("\r\n");
   
-  //CLEAR 2ND SHIFT
+  // CLEAR 2ND SHIFT
   var undoArrayOfSecondShifts = truckArray.slice(0);
   for (var i = 0; i < undoArrayOfSecondShifts.length; i++) {
     undoArrayOfSecondShifts[i] =
@@ -195,7 +225,7 @@ $("#btn-undo").click(function() {
   
   var listOfUndoSecondShiftCommands = undoArrayOfSecondShifts.join("\r\n");
   
-  //RESEST 1ST SHIFT
+  // RESET 1ST SHIFT
   var undoArrayOfFirstShifts = truckArray.slice(0);
   for (var i = 0; i < undoArrayOfFirstShifts.length; i++) {
     undoArrayOfFirstShifts[i] =
@@ -208,7 +238,7 @@ $("#btn-undo").click(function() {
   
   var listOfUndoFirstShiftCommands = undoArrayOfFirstShifts.join("\r\n");
   
-  //RE-USE ON
+  // RE-USE ON
   var arrayOfTrucksForReuseOn = truckArray.slice(0);
   for (var i = 0; i < arrayOfTrucksForReuseOn.length; i++) {
     arrayOfTrucksForReuseOn[i] =
@@ -219,7 +249,7 @@ $("#btn-undo").click(function() {
   
   var listOfReuseOnCommands = arrayOfTrucksForReuseOn.join("\r\n");
   
-  //RE-USE OFF
+  // RE-USE OFF
   var arrayOfTrucksForReuseOff = truckArray.slice(0);
   for (var i = 0; i < arrayOfTrucksForReuseOff.length; i++) {
     arrayOfTrucksForReuseOff[i] =
